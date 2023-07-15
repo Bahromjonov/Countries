0b3a1506bd917ef237a137d929b9f94c4cce630a
Yaxyobek03 <yaxyobekbahromjonov44@gmail.com>
2023-07-15 11:39:17 +0500

    add search

--- app.js
+++ app.js
@@ -1,33 +1,120 @@
-// Dark Mode
+// Elementlarni tanlash
 const elDarkMode = document.querySelector('.header-btn');
 const elSearch = document.querySelector('.search');
-const elSelect = document.querySelector('.hero-select');
-const elLogo = document.querySelector('.logo')
-const elShadow = document.querySelector('.header-bg')
+const elRegionSelect = document.querySelector('.hero-select');
+const elList = document.querySelector('.list');
 
-elDarkMode.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    elSearch.classList.toggle('search-bg');
-  
-    elSelect.classList.toggle('dark-mode');
-    elDarkMode.classList.toggle('dark-mode');
-    elShadow.classList.toggle('header-bg');
+// API URL
+const api = 'https://restcountries.com/v3.1/all';
+
+// Davlatlar ma'lumotlarini saqlash uchun massiv
+let countriesData = [];
+
+// Davlatlarni yuklash va ro'yxatga joylash uchun funksiya
+const fetchCountries = () => {
+  fetch(api)
+    .then((response) => response.json())
+    .then((data) => {
+      countriesData = data;
+      showAllCountries(); // Barcha davlatlarni ko'rsatish
+    })
+    .catch((error) => console.log(error));
+};
+
+// Davlatlarni ko'rsatuvchi funksiya
+const showCountries = (countries) => {
+  elList.innerHTML = ''; // Ro'yxatni tozalash
+
+  countries.forEach((country) => {
+    const li = document.createElement('li');
+    li.classList.add('none');
+    li.classList.add('card');
+   
+    elList.appendChild(li);
+
+    const flagDiv = document.createElement('div');
+    flagDiv.classList.add('flag');
+    flagDiv.style.marginBottom = '24px';
+    const flagImg = document.createElement('img');
+    flagImg.src = country.flags.svg;
+    flagImg.alt = `${country.name.common} flag`;
+    flagImg.style.width = '100%';
+    flagDiv.appendChild(flagImg);
+    li.appendChild(flagDiv);
+
+    const infoDiv = document.createElement('div');
+    infoDiv.classList.add('info');
+    infoDiv.style.paddingBottom = '46px';
+    infoDiv.style.paddingLeft = '24px';
+    li.appendChild(infoDiv);
+
+    const countryName = document.createElement('h3');
+    countryName.style.marginBottom = '16px'
+    countryName.classList.add('still')
+    const countryNameText = document.createTextNode(country.name.common);
+    countryName.appendChild(countryNameText);
+    infoDiv.appendChild(countryName);
+
+    const capital = document.createElement('p');
+    
+    capital.textContent = `Capital: ${country.capital?.[0] || 'N/A'}`;
+    infoDiv.appendChild(capital);
+
+    const population = document.createElement('p');
+   
+    population.textContent = `Population: ${country.population?.toLocaleString() || 'N/A'}`;
+    infoDiv.appendChild(population);
   });
-  
-window.addEventListener('scroll', function() {
-    const elHeader = document.querySelector('.header-bg');
-    const elHero = document.querySelector('.hero');
-    const heroCard = elHero.getBoundingClientRect();
-  
-    if (heroCard.top <= 0) {
-      elHeader.classList.add('sticky');
-      elHero.style.zIndex = '10';
+};
+
+// Barcha davlatlarni ko'rsatish
+const showAllCountries = () => {
+  showCountries(countriesData);
+};
+
+// Bölgelere göre davlatlarni ko'rsatish
+const filterByRegion = (region) => {
+  const filteredCountries = countriesData.filter((country) =>
+    country.region.includes(region)
+  );
+  showCountries(filteredCountries);
+};
+
+// Davlatlarni yuklash va qidirish uchun hodisalar
+fetchCountries();
+
+// Qidirishning ishlayishi uchun hodisa
+elSearch.addEventListener('input', () => {
+  const inputValue = elSearch.value.toLowerCase();
+
+  const cards = document.querySelectorAll('.card');
+  const countryNames = document.querySelectorAll('.info h3');
+
+  cards.forEach((card, i) => {
+    const countryName = countryNames[i].textContent.toLowerCase();
+    if (countryName.includes(inputValue)) {
+      card.style.display = 'block';
     } else {
-      elHeader.classList.remove('sticky');
-      elHero.style.zIndex = 'auto';
+      card.style.display = 'none';
     }
   });
-  
+});
 
-  
-  
+// Bölgelerin seçimi
+elRegionSelect.addEventListener('change', () => {
+  const selectedRegion = elRegionSelect.value;
+
+  if (selectedRegion === 'Filter') {
+    showAllCountries();
+  } else {
+    filterByRegion(selectedRegion);
+  }
+});
+
+// Dark Mode hodisasi
+elDarkMode.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  elSearch.classList.toggle('search-bg');
+  elRegionSelect.classList.toggle('dark-mode');
+  elDarkMode.classList.toggle('dark-mode');
+});
